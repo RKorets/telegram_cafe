d4a95ab09e1ad850610deab16ee1225e3235e2dd
RKorets <93273641+RKorets@users.noreply.github.com>
2022-04-24 20:15:13 +0300

    Update order.py

--- order.py
+++ order.py
@@ -13,36 +13,25 @@ def get_total(bag: list):
             total_sum.append(el[0]*el[1])
     name.append(f'Загальна сума до оплати: {sum(total_sum)} грн.')
     return '\n'.join(name)
-#якшо будем приймати ще код товару то треба буде поміняти логіку
 
 
-
-#dct містить ід клієнта і список кодів товарів в замовленні(або весь список потім туту розберу)
-#має приходити номер замовлення згенерований раніше і список кодів товарів. треба добавляти окремо кожний код товару до одного замовлення
 def db_create_order(order_number: int, user_id: int, order_info: list):
     db = DBConnect()
-    # for el in order_info:
-    #     if type(el) == str:
-    #         db.product_id(el)
-    #     else: pass
-    db.create_order(order_number,user_id)
-    #для Order  order_n  и cust_id
-    #product in order  Orer_n Product_id
-    # print(order_number)
-    # print(user_id)
-    # print(order_info)
+    db.create_order(order_number, user_id)
+    for el in order_info:
+        if type(el) == str:
+            db.create_product_in_order(order_number,db.product_id(el))
+        else:
+            pass
     return
 
 
 def generate_number():
     times = str(time.time_ns())
-    order_number = times[2:12]
-    num = int(order_number)
-    return num
+    order_number = times[2:14]
+    return order_number
 
 
 
 if __name__ == "__main__":
     generate_number()
-
-
