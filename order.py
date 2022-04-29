ca6304c90feaad7dab0777278cc41b01e73742b0
RKorets <93273641+RKorets@users.noreply.github.com>
2022-04-29 23:02:21 +0300

    Update order.py

--- order.py
+++ order.py
@@ -10,7 +10,7 @@ def get_total(bag: list):
         if type(el[0]) == str:
             name.append(f'{el[0]}: {el[1]} шт.')
         else:
-            total_sum.append(el[0]*el[1])
+            total_sum.append(el[0] * el[1])
     name.append(f'Загальна сума до оплати: {sum(total_sum)} грн.')
     return '\n'.join(name)
 
@@ -20,17 +20,15 @@ def db_create_order(order_number: int, user_id: int, order_info: list):
     db.create_order(order_number, user_id)
     for el in order_info:
         if type(el) == str:
-            db.create_product_in_order(order_number,db.product_id(el))
+            db.create_product_in_order(order_number, db.product_id(el))
         else:
             pass
-    return
 
 
 def generate_number():
     times = str(time.time_ns())
     order_number = times[2:14]
     return order_number
-
 
 
 if __name__ == "__main__":
